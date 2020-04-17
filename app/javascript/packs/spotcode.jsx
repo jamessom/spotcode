910d278c54bb7507f1b70401bf642ccaf0b032d4
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-04-17 00:57:54 -0300

    Add react

---
+++ app/javascript/packs/spotcode.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <div>SpotCODE</div>,
+    document.getElementById("root")
+  );
+});
