6d3b02ca06f52433aca2eef29a419ee56bd92b5c
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-04-18 19:17:44 -0300

    Configure react-bulma-components and add content sample

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,7 +1,23 @@
 import React, { Fragment } from 'react';
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import { Hero, Heading, Section, Container } from "react-bulma-components";
+import 'app.scss';
 
 const App = () => {
-  return (<Fragment>SpotCODE</Fragment>);
+  return (<Fragment>
+    <div>
+      <Section>
+        <Hero color="info" >
+          <Hero.Body>
+            <Container>
+              <Heading>SpotCODE</Heading>
+              <Heading subtitle size={3}> React </Heading>
+            </Container>
+          </Hero.Body>
+        </Hero>
+      </Section>
+    </div>
+  </Fragment>);
 };
 
 export default App;
