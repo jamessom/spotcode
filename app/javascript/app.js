090b7163bc58ec7d81c2a78abc01fd0d37935c5a
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-04-19 21:36:06 -0300

    Add route support

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,23 +1,15 @@
 import React, { Fragment } from 'react';
-import "react-bulma-components/dist/react-bulma-components.min.css";
-import { Hero, Heading, Section, Container } from "react-bulma-components";
+import 'react-bulma-components/dist/react-bulma-components.min.css';
 import 'app.scss';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => {
-  return (<Fragment>
-    <div>
-      <Section>
-        <Hero color="info" >
-          <Hero.Body>
-            <Container>
-              <Heading>SpotCODE</Heading>
-              <Heading subtitle size={3}> React </Heading>
-            </Container>
-          </Hero.Body>
-        </Hero>
-      </Section>
-    </div>
-  </Fragment>);
-};
+const App = () => (
+  <Fragment>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Fragment>
+);
 
 export default App;
